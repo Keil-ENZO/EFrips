d91f901ad071a33deeeddac49fbe1e6dad110775
Keil-ENZO <enzo.keil06@icloud.com>
2024-03-21 20:20:53 +0100

    migration bdd

--- Server/migrations/20240320193101-create-blog.js
+++ Server/migrations/20240320193101-create-blog.js
@@ -1,6 +1,54 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "user",
+      },
+      panier: {
+        type: Sequelize.JSON,
+        allowNull: false,
+        defaultValue: [],
+      },
+    });
+
     await queryInterface.createTable("blogs", {
       id: {
         allowNull: false,
@@ -28,5 +76,7 @@ module.exports = {
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable("blogs");
+    await queryInterface.dropTable("users");
+    await queryInterface.dropTable("categories");
   },
 };
